b70d01d6597980ff13191771814670e8d724b845
kylejschulz <kylschulz@gmail.com>
2021-01-04 16:51:50 -0500

    Add minitest to test_helper and create sales_engine class

--- test/sales_engine_test.rb
+++ test/sales_engine_test.rb
@@ -1,14 +1,14 @@
 require_relative './test_helper'
-require_relative './lib/sales_engine'
+require './lib/sales_engine.rb'
 
 class SalesEngineTest < Minitest::Test
   def setup
+    @se = SalesEngine.new
   end
 
   def test_it_exists
-
-  assert_instance_of
-  end
+    assert_instance_of SalesEngine, @se
+    end
   def test_it_has_attributes
   end
   def test_it_can_have_different_attributes
