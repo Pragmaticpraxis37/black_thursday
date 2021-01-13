6d7ab6b0711590cd2db039aad516c83eaf74c947
Jesus Quezada-Guillen <maj3st1@gmail.com>
2021-01-13 16:58:47 -0600

    Update merchant attributes.

--- lib/merchant.rb
+++ lib/merchant.rb
@@ -1,16 +1,22 @@
+require 'time'
 class Merchant
   attr_reader :id,
               :name,
-              :parent
+              :parent,
+              :created_at,
+              :updated_at
 
-  def initialize(id, name, parent)
-    @id = id.to_i
-    @name = name
+  def initialize(args, parent)
+    @id = args[:id].to_i
+    @name = args[:name].to_s
+    @created_at  = Time.parse(args[:created_at].to_s)
+    @updated_at  = Time.parse(args[:updated_at].to_s)
     @parent = parent
   end
 
-  def update(name)
-    @name = name
+  def update(args)
+    @name = (args[:name].to_s) if !args[:name].to_s.nil?
+    @updated_at = Time.now
   end
 
 end
