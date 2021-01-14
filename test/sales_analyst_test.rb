eec90de1c6db2b5fc832f52dafe93d9bd6fd4f31
Jesus Quezada-Guillen <maj3st1@gmail.com>
2021-01-13 18:19:28 -0600

    Add test for #merchants_with_only_one_item.

--- test/sales_analyst_test.rb
+++ test/sales_analyst_test.rb
@@ -167,4 +167,13 @@ class SalesAnalystTest < Minitest::Test
     assert_equal BigDecimal, actual.class
   end
 
+  def test_merchant_with_only_one_item_registered_in_month
+    actual = @sales_analyst.merchants_with_only_one_item('March')
+    assert_equal 21, actual.length
+    assert_equal Merchant, actual.first.class
+    actual = @sales_analyst.merchants_with_only_one_item('June')
+    assert_equal 18, actual.length
+    assert_equal Merchant, actual.first.class
+  end
+
 end
